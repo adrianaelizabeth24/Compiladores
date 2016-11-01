3ee589dc9a3ea82c5cdc2acb477c4e1dc29362c8
Adriana Elizabeth <adrianaelizabeth24@users.noreply.github.com>
2016-11-01 16:54:46 -0600

    modificar clases
    incluyen direcciones de memoria, agregué parametros y tamaño a la tabla
    de funciones

--- tablaFunciones.py
+++ tablaFunciones.py
@@ -1,10 +1,24 @@
 #clase tipo tabla de funciones
 #atributos: nombre y tipo
 class tablaFunciones(object):
-      def __init__(self,nombre,tipo):
+      def __init__(self,nombre,tipo,parametros,size):
           self.nombre = nombre
           self.tipo = tipo
+          self.parametros = parametros
+          self.size = size
       def getNombre(self):
           return self.nombre
       def getTipo(self):
           return self.tipo
+      def getSize(self):
+          return self.size
+      def getParametros(self):
+          return self.parametros
+      def setNombre(self,nombre):
+          self.nombre = nombre
+      def setTipo(self,tipo):
+          self.tipo = tipo
+      def setSize(self,size):
+          self.size = size
+      def setParametros(self,parametros):
+          self.parametros = parametros
